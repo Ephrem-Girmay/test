e3d710e57824f943ca2fb82b4e327b8d7b90dc2a
Ephrem Girmay <ephremgirmay9@gamil.com>
2022-05-20 20:27:39 +0200

    m

--- api/client/src/pages/About/index.jsx
+++ api/client/src/pages/About/index.jsx
@@ -26,8 +26,7 @@ const About = () => {
                     but also the leap into electronic typesetting, remaining
                     essentially unchanged. It was popularised in the 1960s with
                     the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
+                    passages, and more recently with de
                 </P>
             </Info>
         </Main>

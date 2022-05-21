4c5553c7aad9defd47c6e2b502ed73fded090484
Ephrem Girmay <ephremgirmay9@gamil.com>
2022-05-21 19:28:19 +0200

    m

--- api/index.js
+++ api/index.js
@@ -36,10 +36,10 @@ app.use('/carts', cartRoutes);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-// app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 5000, console.log(`Server is up and running`));

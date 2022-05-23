5557669c8a93aeb7fcb211147ad9120474aecb72
Ephrem Girmay <ephremgirmay9@gamil.com>
2022-05-23 12:02:31 +0200

    m

--- api/client/src/pages/About/index.jsx
+++ api/client/src/pages/About/index.jsx
@@ -23,10 +23,9 @@ const About = () => {
                     standard dummy text ever since the 1500s, when an unknown
                     printer took a galley of type and scrambled it to make a
                     type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with de
+                    but also the lremaining essentially unchanged. It was
+                    popularised in the 1960s with the release of Letraset sheets
+                    containing Lorem Ipsum passages, and more recently with de
                 </P>
             </Info>
         </Main>

5490bb7822e0538faa0204fe2692be26adbfc414
Ephrem Girmay <ephremgirmay9@gamil.com>
2022-05-24 22:18:55 +0200

    m

--- api/client/src/pages/About/index.jsx
+++ api/client/src/pages/About/index.jsx
@@ -22,10 +22,9 @@ const About = () => {
                     typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when an unknown
                     printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the lremaining essentially unchanged. It was
-                    popularised in the 1960s with the release of Letraset sheets
-                    containing Lorem Ipsum passages, and more recently with de
+                    type specimen book. Itd not only five centuries, but also
+                    the lremaining essentially unchanged. It was popularised in
+                    the cently with de
                 </P>
             </Info>
         </Main>

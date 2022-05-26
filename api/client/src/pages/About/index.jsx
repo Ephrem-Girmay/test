c29fcb053c8a421e3e1af893db84b76aa076b38b
Ephrem Girmay <ephremgirmay9@gamil.com>
2022-05-26 17:34:39 +0200

    m

--- api/client/src/pages/About/index.jsx
+++ api/client/src/pages/About/index.jsx
@@ -20,9 +20,8 @@ const About = () => {
                 <P>
                     Lorem Ipsum has been the industry's standard dummy text ever
                     since the 1500s, when an unknown printer took a galley of
-                    type and scrambled it to make a type specimen book. Itd not
-                    only five centuries, but also the lremaining essentially
-                    unchanged. It was popularised in the cently with de
+                    type and scrambled it to maining essentially unchanged. It
+                    was popularised in the cently with de
                 </P>
             </Info>
         </Main>
